14eff888ddc3cdec3f6281f0b2afd4891758dab7
Sam Warren <sjwarren91@gmail.com>
2019-07-08 21:33:32 +1000

    click function to move character tile

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,29 @@ var luke = new Character("Luke Skywalker", 5, 100);
 var sidious = new Character("Darth Sidious", 25, 180);
 var maul = new Character("Darth Maul", 20, 150);
 
+
+
 $("#luke").data(luke);
+$("#obiwan").data(obiwan);
+$("#sidious").data(sidious);
+$("#maul").data(maul);
 
 function attack(player, enemy) {
     player.HP -= enemy.AP;
     enemy.HP -= player.AP;
     player.AP += 8;
 }
+
+$(".character").one("click", function(){
+    var x = $(this).detach();
+    x.appendTo(".two");
+    console.log($(this));
+});
+
+
+
+
+
 
 // $("#luke").data("info",{name: "Luke Skywalker", AP: 5, HP: 100});
 // $("#obiwan").data("info",{name: "Obi-wan Kenobi", AP: 8, HP: 120});
@@ -28,23 +44,25 @@ function attack(player, enemy) {
 
 document.onkeyup = function(event) {
 
-    if (obiwan.HP > 0 && luke.HP > 0) {
-        attack(obiwan, luke);
-        console.log("My Health:", obiwan.HP);
-        console.log("My Power:", obiwan.AP);
-        console.log("His Health", luke.HP);
-    }
+    $(".active").detach();
+
+    // if (obiwan.HP > 0 && luke.HP > 0) {
+    //     attack(obiwan, luke);
+    //     console.log("My Health:", obiwan.HP);
+    //     console.log("My Power:", obiwan.AP);
+    //     console.log("His Health", luke.HP);
+    // }
     
-    if (obiwan.HP <= 0) {
-        console.log("You Lose");
-    } else if (luke.HP <= 0) {
-        console.log("You Win");
-    }
+    // if (obiwan.HP <= 0) {
+    //     console.log("You Lose");
+    // } else if (luke.HP <= 0) {
+    //     console.log("You Win");
+    // }
 
 };
 
-var div1 = $("#luke").detach();
-div1.appendTo(".two");
+// var div1 = $("#luke").detach();
+// div1.appendTo(".two");
 
-$("#luke").data(luke);
-$("#test").text($("#luke").data().name);
+// $("#luke").data(luke);
+// $("#test").text($("#luke").data().name);
